00813db272d7b345039997a58e22b308f2bc253a
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-01-25 11:09:19 +0300

    Add .idea

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Режим вывода команд на экран (ECHO) включен.
+a = int(input()) + 1
+b = int(input()) + 5
+if a > b:
+    print("A =", a)
+elif a == b:
+    print("A equal to B")
+else:
+    print("B>A and it's value:", b)
