a80b587c6938c1bd58834110de33e4c9c86a7e03
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-01-25 11:10:38 +0300

    minor changes2

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ b = int(input()) + 5
 if a > b:
     print("A =", a)
 elif a == b:
-    print("A equal to B and value is:",a)
+    print("A equal to B and value is:", a)
 else:
     print("B>A and it's value:", b)
