5b38c32245c66f64884dbaabdae983f5200b4902
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-01-30 15:14:47 +0300

    task4 changes

--- lesson3/task4.py
+++ lesson3/task4.py
@@ -4,4 +4,7 @@ a1 = str(input("Enter 1st digit: "))
 b1 = str(input("Enter 2nd digit: "))
 c1 = str(input("Enter 3rd digit: "))
 text1 = a1+b1+c1
-print("Number of negative digits",text1.count("-"))
+neg1 = text1.count("-")
+pos1 = 3-neg1
+print("Number of negative digits",neg1)
+print("Number of positive digits",pos1)
