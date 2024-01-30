46880c2694a859bcc4e6c60d95d5f55224f9377e
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-01-30 16:17:18 +0300

    task4 — minor changes

--- lesson3/task4.py
+++ lesson3/task4.py
@@ -1,10 +1,7 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
 
-a1 = input("Enter 1st digit: ")
-b1 = input("Enter 2nd digit: ")
-c1 = input("Enter 3rd digit: ")
-text1 = a1+b1+c1
-neg1 = text1.count("-")
-pos1 = 3-neg1
-print("Number of negative digits", neg1)
-print("Number of positive digits", pos1)
+a1 = float(input("Enter 1st digit: "))
+b1 = float(input("Enter 2nd digit: "))
+c1 = float(input("Enter 3rd digit: "))
+print("Number of positive digits", (a1 > 0) + (b1 > 0) + (c1 > 0))
+print("Number of negative digits", (a1 < 0) + (b1 < 0) + (c1 < 0))
