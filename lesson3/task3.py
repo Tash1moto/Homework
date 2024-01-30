b347b6b2a09677e599cd37677acc2a07886605c2
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-01-30 15:05:51 +0300

    task1

---
+++ lesson3/task3.py
@@ -0,0 +1,9 @@
+# Пользователь вводит Имя, Возраст и Город, сформировать приветственное сообщение путем форматирования 3-мя способами
+
+name1 = input("Enter First Name: ")
+age1 = input("Enter your age: ")
+city1 = input("Enter city where you from: ")
+data1 = {"fn": name1, "ua": age1, "cf": city1}
+print("Hello %s %s from %s" % (name1, age1, city1))
+print("Hello %(fn)s %(ua)s from %(cf)s" % data1)
+print(f"Hello {name1} {age1} from {city1}")
