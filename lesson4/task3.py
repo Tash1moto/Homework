b8baaceada2e249b85491a967e0a9f96109468eb
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-02-04 23:43:57 +0300

    lesson4 — task3 ver2

--- lesson4/task3.py
+++ lesson4/task3.py
@@ -2,13 +2,14 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 
-n1 = int(input("Enter Max number: "))
-data1 = (i for i in range(n1+1))
-data2 = set(data1)
-for s in data2:
-    name1 = [input("Enter Name: ")]
-    email1 = [input("Enter Email: ")]
-    name1.extend(email1)
-    ne0 = (name1, email1)
-    dict1 = dict.fromkeys(data2, ne0)
-print(dict1)
+n1 = int(input("Enter Max number of records: "))
+dict1 = {x for x in range(n1)}
+dict2 = {}
+dict0 = {"Name": "NA", "Email": "NA"}
+for key in dict1:
+    name1 = input("Enter Name: ")
+    dict0["Name"] = name1
+    email1 = input("Enter Email: ")
+    dict0["Email"] = email1
+    dict2[key] = dict0
+print(dict2)
