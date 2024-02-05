edca2360a974f1035fed9ddcb3856d20144159b6
viacheslav.grigoriev <viacheslav.grigoriev.91@gmailcom>
2024-02-05 10:05:27 +0300

    initial comment

--- lesson4/task3.py
+++ lesson4/task3.py
@@ -3,14 +3,25 @@
 # для этих ключей будут браться с клавиатуры
 
 n1 = int(input("Enter Max number of records: "))
-dict1 = {x for x in range(n1)}
+# dict1 = {x for x in range(n1)}
+# dict2 = {}
+# dict0 = {"Name": "NA", "Email": "NA"}
+# for key in dict1:
+#     name1 = input("Enter Name: ")
+#     dict0["Name"] = name1
+#     email1 = input("Enter Email: ")
+#     dict0["Email"] = email1
+#     key2 = key+1
+#     dict2[key2] = dict0
+# print(dict2)
+
+dict1 = dict.fromkeys({x for x in range(n1)}, "NA")
 dict2 = {}
 dict0 = {"Name": "NA", "Email": "NA"}
-for key in dict1:
+for key in dict1.keys():
     name1 = input("Enter Name: ")
     dict0["Name"] = name1
     email1 = input("Enter Email: ")
     dict0["Email"] = email1
-    key2 = key+1
-    dict2[key2] = dict0
+    dict2.update({key: dict0.values()})
 print(dict2)
