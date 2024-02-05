59650868dda9373866e8f8bb299f9f98b7c3227a
viacheslav.grigoriev <viacheslav.grigoriev.91@gmailcom>
2024-02-05 21:34:39 +0300

    final fixes

--- lesson4/task3.py
+++ lesson4/task3.py
@@ -4,12 +4,11 @@
 
 n1 = int(input("Enter Max number of records: "))
 dict1 = {x: {"Name": input("Enter Name: "), "Email": input("Enter Email: ")} for x in range(n1)}
-
 print(dict1)
-
 data2 = {}
 for i in range(n1):
         data2[i] = {
                 "Name": input(),
                 "Email": input()
         }
+print(data2)
