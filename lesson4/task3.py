7304e03f71245cec80c3b75408068bfb4a212088
viacheslav.grigoriev <viacheslav.grigoriev.91@gmailcom>
2024-02-05 21:33:11 +0300

    final fixes

--- lesson4/task3.py
+++ lesson4/task3.py
@@ -3,25 +3,13 @@
 # для этих ключей будут браться с клавиатуры
 
 n1 = int(input("Enter Max number of records: "))
-# dict1 = {x for x in range(n1)}
-# dict2 = {}
-# dict0 = {"Name": "NA", "Email": "NA"}
-# for key in dict1:
-#     name1 = input("Enter Name: ")
-#     dict0["Name"] = name1
-#     email1 = input("Enter Email: ")
-#     dict0["Email"] = email1
-#     key2 = key+1
-#     dict2[key2] = dict0
-# print(dict2)
+dict1 = {x: {"Name": input("Enter Name: "), "Email": input("Enter Email: ")} for x in range(n1)}
 
-dict1 = dict.fromkeys({x for x in range(n1)}, "NA")
-dict2 = {}
-dict0 = {"Name": "NA", "Email": "NA"}
-for key in dict1.keys():
-    name1 = input("Enter Name: ")
-    dict0["Name"] = name1
-    email1 = input("Enter Email: ")
-    dict0["Email"] = email1
-    dict2.update({key: dict0})
-print(dict2)
+print(dict1)
+
+data2 = {}
+for i in range(n1):
+        data2[i] = {
+                "Name": input(),
+                "Email": input()
+        }
