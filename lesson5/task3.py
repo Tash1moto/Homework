743ebaf77bbe2df9779b0ed3539189de1c120a05
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-02-06 21:50:09 +0300

    lesson5 — task3

--- lesson5/task3.py
+++ lesson5/task3.py
@@ -1,3 +1,17 @@
 # **Вывести четные числа от 2 до N по 5 в строку
 
-
+n = int(input('Enter MAX: '))
+s = 2
+c = 0
+list1 = []
+while s < n or s == n:
+    if c < 5:
+        s += 1
+        list1.append(s)
+        c += 1
+    elif c == 5:
+        print(list1)
+        c = 0
+        list1 = []
+    else:
+        break
