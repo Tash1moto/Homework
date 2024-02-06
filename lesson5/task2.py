d68769e557fe0bc6aaab8aef954092085f25d0f5
Viacheslav Grigoriev <viacheslav.grigoriev.91@gmail.com>
2024-02-06 21:36:53 +0300

    lesson5 — task2

--- lesson5/task2.py
+++ lesson5/task2.py
@@ -1,4 +1,20 @@
 # Сделать калькулятор: у пользователя
 # спрашивается число, потом действие и второе число
 
-
+fd = float(input("Enter 1st digit: "))
+fa = input("Enter 1st action: ")
+sd = float(input("Enter 2nd digit: "))
+if fa == '+':
+    print(fd + sd)
+elif fa == '-':
+    print(fd - sd)
+elif fa == '*':
+    print(fd * sd)
+elif fa == '/':
+    print(fd / sd)
+elif fa == '^':
+    print(fd ** sd)
+elif fa == '%':
+    print(fd % sd)
+else:
+    print('unsupported action')
